24706191cb09aba5f609d6bcb683713fa0c50199
kai Nishio <kairi240@icloud.com>
2025-07-10 00:28:37 +0900

    入力部分書けた！node問題

--- node_program.java
+++ node_program.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class node_program {
     public static void main(String[] args) {
-
+        Reverse print = new Reverse();
+        print.print_data();
     }
 }
 
@@ -16,11 +17,25 @@ class Node {
     }
 }
 
-class reverse {
-    public Node scan_data(Node data) {
+class Reverse {
+    public void print_data() {
+        Node head = null, current = null;
         Scanner scan = new Scanner(System.in);
-        Node dummy_node = new Node('a');
-        Node current_node = dummy_node;
+        System.out.println("何回入力します？");
+        int num = Integer.parseInt(scan.nextLine());
+        System.out.println("1文字づつ入力してください");
+        for (int i = 1; i <= num; i++) {
+            char enter_data = scan.nextLine().charAt(0);
+            Node new_node = new Node(enter_data);
+            if (head == null) {
+                head = new_node;
+                current = head;
+            } else {
+                current.next = new_node;
+                current = new_node;
+            }
+        }
 
+        scan.close();
     }
 }
