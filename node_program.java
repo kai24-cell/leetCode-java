16adea0428a455d053cad3ec76e2c7c0ac035212
kai Nishio <kairi240@icloud.com>
2025-07-09 23:21:36 +0900

    node問題に取り掛かり始めた

---
+++ node_program.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class node_program {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class reverse {
+    public Node scan_data(Node data) {
+        Scanner scan = new Scanner(System.in);
+        Node dummy_node = new Node('a');
+        Node current_node = dummy_node;
+
+    }
+}
