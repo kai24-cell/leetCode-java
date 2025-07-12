16ea3fdebd71036e626297a8629f84149ddc6ee9
kai Nishio <kairi240@icloud.com>
2025-07-12 10:50:36 +0900

    word問題を解き始めた

---
+++ word_count.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class word_count {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Print_frecently {
+    HashMap<String, Integer> low_and_split_empty = new HashMap<>();
+
+    public void get_and_print() {
+        Scanner scan_data = new Scanner(System.in);
+        String[] word = scan_data.nextLine().toLowerCase().split(" ");
+
+    }
+}
