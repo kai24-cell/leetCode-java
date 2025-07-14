3cb982fbf76a722b666f077f8c4549fbcd04792d
kai Nishio <kairi240@icloud.com>
2025-07-14 22:44:37 +0900

    コミットできてなかったから再コミット

--- word_count.java
+++ word_count.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class word_count {
     public static void main(String[] args) {
@@ -9,10 +11,11 @@ public class word_count {
 
 class Print_frecently {
     HashMap<String, Integer> low_and_split_empty = new HashMap<>();
+    ArrayList<String> low_and_split_word = new ArrayList<>();
 
     public void get_and_print() {
         Scanner scan_data = new Scanner(System.in);
         String[] word = scan_data.nextLine().toLowerCase().split(" ");
-
+        low_and_split_word = new ArrayList<>(Arrays.asList(word));
     }
 }
