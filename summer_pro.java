7fa076501ec9031ea584747a14d3b92c64cdc865
kai Nishio <kairi240@icloud.com>
2025-08-05 23:44:15 +0900

    summerプロジェクト本格的に開始

--- summer_pro.java
+++ summer_pro.java
@@ -6,47 +6,56 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
-public class summer_pro {
+// public class summer_pro {
 
-    public static void main(String[] args) {
-        Cal_point cal = new Cal_point();
-        Cal_point.deal();
-    }
-}
+//     public static void main(String[] args) {
+//         Cal_point cal = new Cal_point();
+//         Cal_point.deal();
+//     }
+// }
 
 @Controller
 class Cal_point {
     @PostMapping("/submit")
     @ResponseBody
     public Integer deal(
-            @RequestParam Double attend_count,
-            @RequestParam Double report_point,
-            @RequestParam Double report_count,
-            @RequestParam List<Double> exam) {
+            @RequestParam Double get_attend_count,
+            @RequestParam Double get_report_point,
+            @RequestParam Double get_report_count,
+            @RequestParam List<Double> get_exam) {
 
     }
 
-    private Integer need_pointper_four(Double report, List exam, Double attend) {// パターンA出席8割レポート4割,期末テスト6割で,まだテスト受けてない
-        Double pass_point = 60.0, four_per = 0.4, report_point;
-        report_point = report * four_per;
-        if (report_point < 0 || report_point > 40) {
-            continue;
+    private Double cal_report_point(Double report, Double report_count) {
+        Double four_per = 0.4, report_point;
+        boolean result;
+        report_point = report * four_per / report_count;
+        Err_check check = new Err_check();
+        result = check.report_point_check(report_point);
+        if (result == true) {
+            return report_point;
+        } else {
+            throw new IllegalArgumentException("エラー:レポート点の入力が上手くいってません。");
         }
+    }
 
-        return;
+    private Double need_point(Double report_count, Double report, List<Double> exam, Double attend) {// パターンA出席8割レポート4割,期末テスト6割で,まだテスト受けてない
+        Double pass_point = 60.0, four_per = 0.4, report_point, need_least_point, low_check = 0.0, high_check = 40.0;
+        report_point = this.cal_report_point(report, report_count);
+        need_least_point = pass_point - report_point;
+
+        return need_least_point;
     }
 
 }
 
-class Test_base {// オーバーライド用親クラス
-    private Integer need_pointper_four(Double report, List exam, Double attend) {// パターンA出席8割レポート4割,期末テスト6割で,まだテスト受けてない
-        Double pass_point = 60.0, four_per = 0.4, report_point;
-        report_point = report * four_per;
-        if (report_point < 0 || report_point > 40) {
-            continue;
+class Err_check {
+    public boolean report_point_check(Double report_point) {
+        Double low_check = 0.0, high_check = 40.0;
+        if (report_point < low_check || report_point > high_check) {
+            return false;
+        } else {
+            return true;
         }
-
-        return;
     }
-
 }
