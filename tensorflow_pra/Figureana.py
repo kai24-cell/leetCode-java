3d58b1111b8d84e6eabe4ef2b8c3b7cae6e601b9
kai Nishio <kairi240@icloud.com>
2025-08-19 23:53:29 +0900

    ノードの数を増やして実験してみた

--- tensorflow_pra/Figureana.py
+++ tensorflow_pra/Figureana.py
@@ -1,21 +1,21 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+import numpy as np
 
-# データ読み込み（TensorFlow内蔵）
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0  # 正規化
+# XORデータ
+x = np.array([[0,0],[0,1],[1,0],[1,1]])#入力
+y = np.array([0,1,1,0])  # XORの答え
 
-# モデル構築
-model = models.Sequential([
-    layers.Flatten(input_shape=(28, 28)),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(10, activation='softmax')
+# モデル
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(8, activation="relu",input_shape=(2,)),
+    tf.keras.layers.Dense(1, activation="sigmoid")
 ])
 
-# コンパイル
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer="adam",
+              loss="binary_crossentropy",
+              metrics=["accuracy"])
 
-# 学習
-model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+model.fit(x, y, epochs=500, verbose=1)
+
+print("予測:")
+print(model.predict(x))
