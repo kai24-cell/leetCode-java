e9299449792fbece275655e10e65be96e48ac0d5
kai Nishio <kairi240@icloud.com>
2025-08-20 23:44:14 +0900

    andもやってみた。機械学習

--- tensorflow_pra/Figureana.py
+++ tensorflow_pra/Figureana.py
@@ -7,7 +7,7 @@ y = np.array([0,1,1,0])  # XORの答え
 
 # モデル
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(8, activation="relu",input_shape=(2,)),
+    tf.keras.layers.Dense(8, activation="relu",input_shape=(2,)),#ノードの数
     tf.keras.layers.Dense(1, activation="sigmoid")
 ])
 
