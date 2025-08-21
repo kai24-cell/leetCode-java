f2cd44b6081ea5f1701987e7c2d5aaa9cd218af0
kai Nishio <kairi240@icloud.com>
2025-08-21 23:43:54 +0900

    orも書いてみた

---
+++ tensorflow_pra/or.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+
+x1=np.array([[0,0],[0,1],[1,0],[1,1]],dtype=np.float32)
+y1=np.array([0,1,1,0],dtype=np.float32)
+
+model=tf.keras.Sequential([
+    tf.keras.layers.Dense(8, activation="relu",input_shape=(2,)),
+    tf.keras.layers.Dense(1,activation="sigmoid")
+])
+
+model.compile(optimizer="adam",
+               loss="binary_crossentropy",
+               metrics=["accuracy"])
+
+model.fit(x1,y1,epochs=500,verbose=0)
+print("予測")
+print(model.predict(x1))
