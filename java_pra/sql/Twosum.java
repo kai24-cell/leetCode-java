16d3475fe7b51f6dc37de04adebd6a1b732c389a
kai Nishio <kairi240@icloud.com>
2025-08-30 23:41:59 +0900

    twosumに挑戦中

---
+++ java_pra/sql/Twosum.java
@@ -0,0 +1,21 @@
+public class Twosum {
+    public static void main(String[] args) {
+        int nums[] = { 2, 7, 11, 15 }, output[];
+        int taeget = 9;
+        Solutions x = new Solutions();
+        output = x.twoSum(nums, taeget);
+
+    }
+}
+
+class Solutions {
+    public int[] twoSum(int[] num, int target) {
+        int sum = 0, output[];
+        while (sum != target) {
+            for (int i = 0; i <= num.length; i++) {
+
+            }
+        }
+        return output;
+    }
+}
